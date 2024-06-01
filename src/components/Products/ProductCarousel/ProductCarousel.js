ca3b0b90a87e0d4d0c820917af70028aa1bd1851
Kith933UTH <nguyenkiet51569@gmail.com>
2024-06-01 07:53:08 +0700

    Fix Product Carousel Nav

--- src/components/Products/ProductCarousel/ProductCarousel.js
+++ src/components/Products/ProductCarousel/ProductCarousel.js
@@ -8,7 +8,7 @@ const ProductCarousel = ({ imgList }) => {
 			autoplayDelay={5000}
 			loop={true}
 			navigation={({ setActiveIndex, activeIndex, length }) => (
-				<div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
+				<div className="absolute bottom-0 left-2/4 z-50 flex -translate-x-2/4 gap-2">
 					{new Array(length).fill('').map((_, i) => (
 						<span
 							key={i}
